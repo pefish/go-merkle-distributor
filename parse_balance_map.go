32b4813331c23d9b2993225bec2f07e58d64f647
pefish <darrondarron@qq.com>
2023-07-24 16:29:52 +0800

    v0.1.2

--- parse_balance_map.go
+++ parse_balance_map.go
@@ -7,9 +7,10 @@ import (
 )
 
 type Claim struct {
-	Index  int           `json:"index"`
-	Amount string        `json:"amount"`
-	Proof  []common.Hash `json:"proof"`
+	Index   int            `json:"index"`
+	Account common.Address `json:"account"`
+	Amount  string         `json:"amount"`
+	Proof   []common.Hash  `json:"proof"`
 }
 
 type MerkleDistributorInfo struct {
@@ -37,9 +38,10 @@ func ParseBalanceMap(balances []Balance) (MerkleDistributorInfo, error) {
 		tokenTotal = big.NewInt(0).Add(tokenTotal, balance.Amount)
 
 		info.Claims = append(info.Claims, Claim{
-			Index:  idx,
-			Amount: "0x" + balance.Amount.Text(16),
-			Proof:  proof,
+			Index:   idx,
+			Account: balance.Account,
+			Amount:  "0x" + balance.Amount.Text(16),
+			Proof:   proof,
 		})
 	}
 
